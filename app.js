7f9c10ffa748c7599f24bc3881928aec38e3bafa
Abdulqadir1232 <100140773+Abdulqadir1232@users.noreply.github.com>
2022-04-07 16:56:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const { pool } = require("./db");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("dotenv").config();
+const {generateAccessToken} = require("./middleware/auth")
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+// Middleware
+
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.set("view engine", "ejs");
+
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: process.env.SESSION_SECRET,
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/", (req, res) => {
+  //   res.render("index");
+  console.log("index")
+});
+
+app.post("/users/register", async (req, res) => {
+  try {
+    const { firstname,lastname, email, password } = req.body;
+    const userExist = await new Promise((resolve, reject) => {
+      pool.query('SELECT * from users1 where email = ($1)', [email], (error, result) => {
+        if (error) {
+          throw error;
+        }
+        resolve(result.rowCount)
+      })
+    })
+    if (userExist > 0) {
+      return res.send("User already exists");
+    }
+    await new Promise((resolve, reject) => {
+      pool.query('INSERT INTO users1 (firstname,lastname,email,password) VALUES ($1, $2, $3, $4)', [firstname,lastname, email,password], (error, result) => {
+        if (error) {
+          throw error
+        }
+        resolve()
+      })
+    })
+    return res.send("User registered successfully")
+  } catch (error) {
+    return res.send("error")
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// login api
+app.post("/users/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user=  await new Promise((resolve, reject) => {
+    pool.query('SELECT * from users1 where (email,password) = ($1,$2)', [email,password], (error, result) => {
+      if (error) {
+
+        throw error;
+      }
+      console.log(result.rows,"rows are here");
+       resolve(result.rows)
+    })
+  })
+  console.log(user)
+   
+    if(user[0].email==email){
+      console.log(user[0])
+      if(user[0].password==password){
+    let accessToken=    generateAccessToken({
+          userId:user[0].id,
+          firstName:user[0].firstname
+        })
+        console.log(accessToken,'accesstoken');
+        return res.send({accessToken});
+      }
+      else{
+        return res.send("Your pasword is wrong");
+      }
+
+    }
+    else{
+      return res.send("Your email is invalid");
+    }
+ 
+  } catch (error) {
+    console.log(error)
+    return res.send("error")
+  }
+
+});
+
+
+app.listen(PORT, () => {
+  console.log("your server start")
+})
---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE sing;
+
+CREATE TABLE sing_in(
+    sing_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
